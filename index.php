a9f41ff5f848b1d4e4c8ff3f372868654dfadcae
mawann <35596783+mawann@users.noreply.github.com>
2023-05-20 19:58:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// Jangan ubah apa pun yang ada di file ini.
+// Untuk mengetik serial number, ketik di file config.php.
+
+define('CLI_SCRIPT', true);
+
+require('/config.php');
+
+$url = "https://www.mawan.net/moodle/get/token/";
+$wwwroot = $CFG->wwwroot;
+$password = password_hash($wwwroot . '/' . ((isset($CFG->mmqpc) ? $CFG->mmqpc : 'tes'), PASSWORD_DEFAULT);
+
+// Data yang ingin dikirim
+$postData = array(
+    'wwwroot' => $wwwroot,
+    'password' => $password
+);
+
+// Inisialisasi CURL
+$ch = curl_init($url);
+
+// Set opsi CURL
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Eksekusi CURL
+$response = curl_exec($ch);
+
+// Cek jika ada kesalahan
+if(curl_errno($ch)){
+    echo 'Error: ' . curl_error($ch);
+}
+
+// Tutup CURL
+curl_close($ch);
+
+echo "Respon Mawan.net adalah:" . PHP_EOL;
+echo $response;
+echo PHP_EOL;
